fa83014a236c8967b06071178e4d2d7e4d8ad377
hesham <heshamali1995@hotmail.com>
2023-08-29 16:21:25 +0300

    Test

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  hasUpdate = false;
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit(): void {
+    // check for platform update
+    if (this.swUpdate.isEnabled) {
+      interval(600).subscribe(() =>
+        this.swUpdate.checkForUpdate().then((resp) => {
+          console.log(resp);
+        })
+      );
+    }
+    this.swUpdate.available.subscribe(() => {
+      this.hasUpdate = true;
+    });
+  }
+
+  reloadSite(): void {
+    location.reload();
+  }
+}
