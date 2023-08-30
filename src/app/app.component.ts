7a92864f430fef97f15457eed6fdd6393332e230
hesham <heshamali1995@hotmail.com>
2023-08-30 11:38:10 +0300

    SSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ApplicationRef, Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { interval } from 'rxjs';
 
@@ -30,7 +30,10 @@ import { interval } from 'rxjs';
     location.reload();
   }
 } */
-export class AppComponent implements OnInit {
+
+// Activated Updates With Reloading The Page
+
+/* export class AppComponent implements OnInit {
   constructor(private swUpdate: SwUpdate) {
     this.updateClient();
   }
@@ -60,6 +63,52 @@ export class AppComponent implements OnInit {
         'Available2: ',
         JSON.stringify(event.current)
       );
+    });
+  }
+} */
+
+// Check For Updates
+export class AppComponent implements OnInit {
+  constructor(private swUpdate: SwUpdate, private appRef: ApplicationRef) {
+    this.updateClient();
+  }
+  ngOnInit(): void {}
+  updateClient() {
+    if (!this.swUpdate.isEnabled) {
+      console.log('Not Enabled');
+      return;
+    }
+    this.swUpdate.available.subscribe((event) => {
+      console.log(
+        'current: ',
+        JSON.stringify(event.current),
+        'available',
+        JSON.stringify(event.available)
+      );
+      if (confirm('Update Available For the app please confirm')) {
+        this.swUpdate.activateUpdate().then(() => {
+          window.location.reload();
+        });
+      }
+    });
+    this.swUpdate.activated.subscribe((event) => {
+      console.log(
+        'Current2: ',
+        JSON.stringify(event.previous),
+        'Available2: ',
+        JSON.stringify(event.current)
+      );
+    });
+  }
+  checkUpdate() {
+    this.appRef.isStable.subscribe((resp) => {
+      if (resp) {
+        const timeInterval = interval(1000);
+        timeInterval.subscribe(() => {
+          this.swUpdate.checkForUpdate().then(() => console.log('Checked'));
+          console.log('Update Checked');
+        });
+      }
     });
   }
 }
