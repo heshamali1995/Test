f50d16fd44971a33f034bf520d98cb86cb23e6e3
hesham <heshamali1995@hotmail.com>
2023-09-01 18:09:54 +0300

    LS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,12 @@ import { interval } from 'rxjs';
 })
 export class AppComponent {
   // For Checking Updated Every Specific Time
-  /*   hasUpdate = false; */
-  /*   constructor(private swUpdate: SwUpdate) {} */
+  hasUpdate = false;
+  constructor(private swUpdate: SwUpdate) {}
 
   ngOnInit(): void {
     // check for platform update
-    /*     if (this.swUpdate.isEnabled) {
+    if (this.swUpdate.isEnabled) {
       interval(1000).subscribe(() =>
         this.swUpdate.checkForUpdate().then((event) => {
           console.log(`Check For Updates ${event}`);
@@ -24,12 +24,12 @@ export class AppComponent {
     this.swUpdate.available.subscribe((event) => {
       console.log(`Current `, event.current, ' Available ', event.available);
       this.hasUpdate = true;
-    }); */
+    });
   }
 
-  /*   reloadSite(): void {
+  reloadSite(): void {
     location.reload();
-  } */
+  }
 }
 
 // 1 => Activated Updates With Reloading The Page
